6565e8f3bb770f1b5610443b7d363ded7a9a7133
zhangyong <zhangyong01@kanzhun.com>
2023-06-07 10:18:41 +0800

    clear web server

--- src/main/java/com/zy/integrate/IntegrateApplication.java
+++ src/main/java/com/zy/integrate/IntegrateApplication.java
@@ -1,15 +1,9 @@
 package com.zy.integrate;
 
-import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
-import com.alicp.jetcache.anno.config.EnableMethodCache;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@MapperScan("com.zy.integrate.*")
-@EnableMethodCache(basePackages = "com.zy.integrate.service") //支持方法缓存的包
-@EnableCreateCacheAnnotation
 @SpringBootApplication
 @EnableAsync
 public class IntegrateApplication {
